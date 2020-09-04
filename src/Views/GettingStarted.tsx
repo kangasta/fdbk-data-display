de94082fa686fd20d847fd530e980b57b5f6d04e
Toni Kangas <toni.ar.kangas@gmail.com>
2020-09-04 22:02:07 +0300

    Add component for demo charts

--- src/Views/GettingStarted.tsx
+++ src/Views/GettingStarted.tsx
@@ -37,11 +37,11 @@ const getRandomNumberWithTimestamp = (
   y: Math.random() * max,
 });
 
-export const GettingStarted = (): React.ReactElement => {
+const DemoCharts = (): React.ReactElement => {
   const [doughnutValues, setDoughnutValues] = useState<number[]>(
     getRandomNumbers(4)
   );
-  const [lineValues, setLineValues] = useState<TimePoint[][]>([[], [], [], []]); // LABELS.map(() => [getRandomNumberWithTimestamp()])
+  const [lineValues, setLineValues] = useState<TimePoint[][]>([[], [], [], []]);
 
   useEffect(() => {
     const randomizeDoughnut = () => setDoughnutValues(getRandomNumbers(4));
@@ -64,26 +64,7 @@ export const GettingStarted = (): React.ReactElement => {
   }, [setDoughnutValues, setLineValues]);
 
   return (
-    <Page>
-      <Typography variant="h5" component="h2">
-        Getting started
-      </Typography>
-      <p>
-        Hello!{' '}
-        <span role="img" aria-label="waving hand">
-          👋
-        </span>
-      </p>
-      <p>
-        You are seeing this demo page because this data display has not been
-        configured to fetch data from any API. In order to configure API and
-        authentication for this data display, navigate to the settings page.
-        Alternatively, if you are the admin of this page, you can configure the
-        default settings through a <code>config.js</code> file. This{' '}
-        <code>config.js</code> file should be located in the same directory as
-        the <code>index.html</code>.
-      </p>
-      <p>If you are here to see the demo charts, kick back and relax.</p>
+    <>
       <ChartContainer
         field="Pie chart"
         type="doughnut"
@@ -94,6 +75,31 @@ export const GettingStarted = (): React.ReactElement => {
         type="line"
         data={getLineData(lineValues)}
       />
-    </Page>
+    </>
   );
 };
+
+export const GettingStarted = (): React.ReactElement => (
+  <Page>
+    <Typography variant="h5" component="h2">
+      Getting started
+    </Typography>
+    <p>
+      Hello!{' '}
+      <span role="img" aria-label="waving hand">
+        👋
+      </span>
+    </p>
+    <p>
+      You are seeing this demo page because this data display has not been
+      configured to fetch data from any API. In order to configure API and
+      authentication for this data display, navigate to the settings page.
+      Alternatively, if you are the admin of this page, you can configure the
+      default settings through a <code>config.js</code> file. This{' '}
+      <code>config.js</code> file should be located in the same directory as the{' '}
+      <code>index.html</code>.
+    </p>
+    <p>If you are here to see the demo charts, kick back and relax.</p>
+    <DemoCharts />
+  </Page>
+);
