f0f64a3310ec3d0ef07a622534efe7109b3a7490
Toni Kangas <toni.ar.kangas@gmail.com>
2020-12-05 14:38:00 +0200

    Use memoized value for auth header to avoid re-fetch loop

--- src/Utils/testUtils.tsx
+++ src/Utils/testUtils.tsx
@@ -7,6 +7,13 @@ export const TEST_ID_NAME = 'Test Dude';
 export const TEST_ID_TOKEN =
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwiZ2l2ZW5fbmFtZSI6IlRlc3QiLCJmYW1pbHlfbmFtZSI6IkR1ZGUiLCJpYXQiOjE1MTYyMzkwMjJ9.SRHYhnDAuP2qB1dHTo4-aCS8gO5nTpzZKEIKXdtMVdQ';
 
+export const AUTHENTICATION_STATE = {
+  id_token: TEST_ID_TOKEN,
+  access_token: 'ACCESS_TOKEN',
+  expires_in: 3600,
+  token_type: 'Bearer',
+};
+
 export const getKeyUpEvent = (value: string, key = 'Enter') => ({
   key,
   target: { value },
---
+++ src/__tests__/Utils/authHeaderHook.test.tsx
@@ -0,0 +1,53 @@
+import React, { useEffect } from 'react';
+import { createStore } from 'redux';
+import { render } from '@testing-library/react';
+
+import { AUTHENTICATION_STATE, TestWrapper } from '../../Utils/testUtils';
+import { useAuthorizationHeader, Headers } from '../../Utils/authHeaderHook';
+import mainReducer from '../../Reducers/main';
+import { setAuthentication } from '../../Utils/actionCreators';
+
+interface HeaderTestComponentProps {
+  changedCallback: (headers?: Headers) => void;
+  anotherProp: number;
+}
+const HeaderTestComponent = ({
+  changedCallback,
+  anotherProp,
+}: HeaderTestComponentProps) => {
+  const header = useAuthorizationHeader();
+
+  useEffect(() => {
+    changedCallback(header);
+  }, [changedCallback, header]);
+
+  return (
+    <div>
+      <h1>Render {anotherProp}</h1>
+      {JSON.stringify(header)}
+    </div>
+  );
+};
+
+it('Headers value does not change if token does not change', () => {
+  const callback = jest.fn();
+
+  const store = createStore(mainReducer);
+  store.dispatch(setAuthentication(AUTHENTICATION_STATE));
+
+  const { rerender } = render(
+    <TestWrapper store={store}>
+      <HeaderTestComponent changedCallback={callback} anotherProp={1} />
+    </TestWrapper>
+  );
+
+  expect(callback).toHaveBeenCalled();
+
+  rerender(
+    <TestWrapper store={store}>
+      <HeaderTestComponent changedCallback={callback} anotherProp={2} />
+    </TestWrapper>
+  );
+
+  expect(callback).toHaveBeenCalledTimes(1);
+});
