e376d0b3a64188f7cd678eeb3e6053cf44f3ac37
Toni Kangas <toni.ar.kangas@gmail.com>
2020-12-11 01:21:21 +0200

    Add first cypress test

---
+++ Dockerfile
@@ -0,0 +1,21 @@
+FROM node:lts-alpine as build
+
+RUN apk add git
+WORKDIR /app
+
+COPY ./package* /app/
+RUN npm i
+
+COPY . /app
+RUN export TAG=$(git name-rev --tags --name-only $(git rev-parse HEAD)) && \
+    export TAG=$(test $TAG != undefined && echo $TAG || echo '') && \
+    echo "TAG=$TAG" > ".env" && \
+    echo "COMMIT=$(git rev-parse HEAD)" >> ".env"
+RUN npm run build
+
+
+FROM nginx:alpine
+
+COPY ./nginx.conf /etc/nginx/nginx.conf
+COPY --from=build /app/dist /app
+COPY ./config.js /app/config.js
---
+++ cypress/integration/settings.spec.ts
@@ -0,0 +1,40 @@
+/// <reference types="cypress" />
+/// <reference types="@testing-library/cypress" />
+
+const setApiUrl = (save=false) => {
+  cy.findAllByTestId('settings-view-toggle-button')
+    .click();
+
+  cy.findByLabelText('Statistics URL')
+    .type('/api/statistics');
+
+  if (save) {
+    cy.findByText('Save to local storage')
+      .click();
+  }
+
+  cy.findAllByTestId('settings-view-toggle-button')
+    .click();
+}
+
+context('Settings', (): void => {
+  before((): void => {
+    cy.visit('http://localhost:3000');
+  });
+
+  it('allows saving settings to local storage', (): void => {
+    cy.contains('Getting started');
+
+    setApiUrl();
+    cy.contains('Was not able to fetch data from the server.');
+
+    cy.reload()
+    cy.contains('Getting started');
+
+    setApiUrl(true);
+    cy.contains('Was not able to fetch data from the server.');
+
+    cy.reload()
+    cy.contains('Was not able to fetch data from the server.');
+  });
+});
