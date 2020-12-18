d1bcbced907933ea32832ddb34dbdeab3b4b5b55
Toni Kangas <toni.ar.kangas@gmail.com>
2020-12-18 21:55:33 +0200

    Analyze cypress tests with eslint

--- cypress/integration/settings.spec.ts
+++ cypress/integration/settings.spec.ts
@@ -1,21 +1,17 @@
 /// <reference types="cypress" />
 /// <reference types="@testing-library/cypress" />
 
-const setApiUrl = (save=false) => {
-  cy.findAllByTestId('settings-view-toggle-button')
-    .click();
+const setApiUrl = (save = false) => {
+  cy.findAllByTestId('settings-view-toggle-button').click();
 
-  cy.findByLabelText('Statistics URL')
-    .type('/api/statistics');
+  cy.findByLabelText('Statistics URL').type('/api/statistics');
 
   if (save) {
-    cy.findByText('Save to local storage')
-      .click();
+    cy.findByText('Save to local storage').click();
   }
 
-  cy.findAllByTestId('settings-view-toggle-button')
-    .click();
-}
+  cy.findAllByTestId('settings-view-toggle-button').click();
+};
 
 context('Settings', (): void => {
   before((): void => {
@@ -28,13 +24,13 @@ context('Settings', (): void => {
     setApiUrl();
     cy.contains('Was not able to fetch data from the server.');
 
-    cy.reload()
+    cy.reload();
     cy.contains('Getting started');
 
     setApiUrl(true);
     cy.contains('Was not able to fetch data from the server.');
 
-    cy.reload()
+    cy.reload();
     cy.contains('Was not able to fetch data from the server.');
   });
 });
